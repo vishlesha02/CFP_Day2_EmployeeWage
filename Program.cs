2e9e7edcf1e4bb816be1d86df5d2690917eca7ef
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-12 10:45:11 +0530

    Added: UC1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
