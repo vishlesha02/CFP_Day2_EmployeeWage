c15e1f5e9373fb46be0cfac92084662762685e60
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-12 13:29:23 +0530

    Added: UC5

--- Program.cs
+++ Program.cs
@@ -11,32 +11,38 @@ namespace EmployeeWage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-            Random random = new Random();
-
-            int empCheck = random.Next(0,3);
-
-            switch (empCheck)
+            for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                empHrs = 4;
-                    break;
+                Random random = new Random();
 
-                case IS_FULL_TIME:
-                empHrs = 8;
-                    break;
+                int empCheck = random.Next(0, 3);
 
-                deafault:
-                    empHrs = 0;
-                    break;
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine($"Emp Wage : {empWage}");
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine($"Emp Wage : {empWage}");
-
+            Console.WriteLine($"Total Employee Wage : {totalEmpWage}");
         }
     }
 }
