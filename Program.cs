4356314470c97bdafdb77adedd8f508e9ba1e467
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-12 12:14:07 +0530

    Added: UC2

--- Program.cs
+++ Program.cs
@@ -11,18 +11,28 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
+
             int empCheck = random.Next(0,2);
 
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else 
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine($"Emp Wage : {empWage}");
+
         }
     }
 }
