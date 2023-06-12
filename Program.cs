c44760c670e6b75e96f8175add4fc61d7b3d7f62
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-12 12:39:24 +0530

    Added: UC4

--- Program.cs
+++ Program.cs
@@ -8,12 +8,11 @@ namespace EmployeeWage
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-
             int empHrs = 0;
             int empWage = 0;
 
@@ -21,17 +20,19 @@ namespace EmployeeWage
 
             int empCheck = random.Next(0,3);
 
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
+                case IS_PART_TIME:
                 empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
+                    break;
+
+                case IS_FULL_TIME:
                 empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                    break;
+
+                deafault:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine($"Emp Wage : {empWage}");
