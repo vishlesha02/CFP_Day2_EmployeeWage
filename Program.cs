28af547385c5e505bf32d3ef7705ed297e1dd608
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-12 13:48:30 +0530

    Added: UC6

--- Program.cs
+++ Program.cs
@@ -12,13 +12,16 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUMBER_OF_WORKING_DAYS = 20;
+        public const int MAX_HR_IN_MONTH = 100;
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
+            int day = 1;
+            int Total_Emp_Hr = 0;
 
-            for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++)
+            while (day <= NUMBER_OF_WORKING_DAYS && empHrs < MAX_HR_IN_MONTH)
             {
                 Random random = new Random();
 
@@ -40,7 +43,10 @@ namespace EmployeeWage
                 }
                 empWage = empHrs * EMP_RATE_PER_HOUR;
                 totalEmpWage += empWage;
-                Console.WriteLine($"Emp Wage : {empWage}");
+                Total_Emp_Hr += empHrs;
+
+                Console.WriteLine($"Day {day} Employee Wage : {empWage} Total Working Hr : { Total_Emp_Hr} ");
+                day++;
             }
             Console.WriteLine($"Total Employee Wage : {totalEmpWage}");
         }
