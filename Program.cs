3e476fd38f236ee10e863e90b290c24a1f078b05
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-12 12:30:53 +0530

    Added: UC3

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
@@ -18,16 +19,18 @@ namespace EmployeeWage
 
             Random random = new Random();
 
-            int empCheck = random.Next(0,2);
+            int empCheck = random.Next(0,3);
 
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PART_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
                 empHrs = 8;
             }
-            else 
+            else
             {
-                Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
