e2dd753024bdc3e96333add5b678a5cb4e124977
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-13 00:30:40 +0530

    Added: ucc9

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,22 @@ namespace EmployeeWage
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-      
-        public void ComputeWage(String Company, int EMP_RATE_PER_HOUR, int NUMBER_OF_WORKING_DAYS, int MAX_HR_IN_MONTH)
+
+        private String Company;
+        private int EMP_RATE_PER_HOUR;
+        private int NUMBER_OF_WORKING_DAYS;
+        private int MAX_HR_IN_MONTH;
+
+        public Program(String Company, int EMP_RATE_PER_HOUR, int NUMBER_OF_WORKING_DAYS, int MAX_HR_IN_MONTH)
+        {
+            this.Company= Company;
+            this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+            this.NUMBER_OF_WORKING_DAYS = NUMBER_OF_WORKING_DAYS;
+            this.MAX_HR_IN_MONTH = MAX_HR_IN_MONTH;
+
+        }
+
+        public void ComputeWage()
         { 
             int empHrs = 0;
             int empWage = 0;
@@ -19,7 +34,7 @@ namespace EmployeeWage
             int day = 1;
             int total_Emp_Hr = 0;
 
-            while (day <= NUMBER_OF_WORKING_DAYS && empHrs < MAX_HR_IN_MONTH)
+            while (day <= this.NUMBER_OF_WORKING_DAYS && empHrs < this.MAX_HR_IN_MONTH)
             {
                 Random random = new Random();
 
@@ -39,7 +54,7 @@ namespace EmployeeWage
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
+                empWage = empHrs * this.EMP_RATE_PER_HOUR;
                 totalEmpWage += empWage;
                 total_Emp_Hr += empHrs;
 
@@ -47,14 +62,15 @@ namespace EmployeeWage
                 day++;
             
             }
-            Console.WriteLine($"Total Employee Wage for {Company} : {totalEmpWage}");
+            Console.WriteLine($"Total Employee Wage for {this.Company} : {totalEmpWage}");
         }
 
         public static void Main(String[] args)
         {
-            Program c = new Program();
-            c.ComputeWage("Jio", 20, 3, 12);
-            c.ComputeWage("Big-Baazar", 30, 2, 11);
+            Program jio = new Program("Jio", 20, 3, 12);
+            jio.ComputeWage();
+            Program bigbazar = new Program("Big-Baazar", 30, 2, 11);
+            bigbazar.ComputeWage();
 
 
         }
